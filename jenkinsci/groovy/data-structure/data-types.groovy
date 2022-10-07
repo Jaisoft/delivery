4fb9781584f7fa453c6cbe4241a3a97010208cd0
Jaisoft <jaime.gomez.moraleda@gmail.com>
2022-10-07 15:22:11 +0200

    add pocs

---
+++ jenkinsci/groovy/data-structure/data-types.groovy
@@ -0,0 +1,37 @@
+
+a = 2
+
+println a
+
+def b = 2
+
+println b
+
+int c = 3
+
+println c
+
+x = "Esto es un String"
+println x.class
+println x
+
+y = 12
+println y.class
+
+println y
+
+// Defining a variable in lowercase  
+int h = 5;
+
+// Defining a variable in uppercase 
+int C = 6; 
+	  
+// Defining a variable with the underscore in it's name 
+def _Name = "Joe"; 
+		
+println(c); 
+
+println(C); 
+
+println(_Name); 
+
--- jenkinsci/groovy/data-structure/variables.groovy
+++
@@ -1,6 +0,0 @@
-x = "Esto es un String"
-println x.class
-println x
-x = 12
-println x.class
-println x
