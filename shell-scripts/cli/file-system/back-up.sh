48a1fe278dbc3c3cc13fb85fba545d25659ae19a
Jaime <jaime.gomez.moraleda@gmail.com>
2022-10-30 19:05:54 +0100

    xonsh

--- shell-scripts/cli/file-system/back-up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-OF=delivery_backup_$(date +%Y%m%d).tar.gz
-tar -czf $OF /home/jaime/Documents/delivery
---
+++ xonsh/xonsh-script2.xsh
@@ -0,0 +1,5 @@
+$GOAL = 'Become the Lord of the Files'
+
+print($GOAL)
+
+del $GOAL
